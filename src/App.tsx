cd12902857812170efc80c30986709c5b99e37b9
Nirupama Singh <nirupama.singh@iarc.co.in>
2025-01-28 21:07:35 +0530

    added vite , tailwind and ant design

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='h-screen bg-black flex justify-center items-center'>
+      <h1 className=' text-white text-5xl font-bold'>Knock Out Task</h1>
+    </div>
+  )
+}
+
+export default App
